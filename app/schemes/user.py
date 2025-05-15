36222d465f34f8fc3e4706719acc4442afc95a11
Evgen-Jekov <zekovevgen992@gmail.com>
2025-05-15 01:02:57 +0000

    UserInput create

---
+++ app/schemes/user.py
@@ -0,0 +1,25 @@
+from typing import Literal
+import re
+
+from pydantic import BaseModel, field_validator, EmailStr
+
+
+class UserInput(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    role: Literal['user', 'admin']
+
+    @field_validator('password')
+    def password_validate(cls, value : str) -> str:
+        pattern = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$'
+
+        if re.match(pattern=pattern, string=value) or len(value) >= 100:
+            raise ValueError('Username or password does not match')
+        
+        return value
+    
+    @field_validator('username')
+    def username_validate(cls, value : str) -> str:
+        if len(value) < 5 or len(value) >= 50:
+            raise ValueError('Username validation error')
